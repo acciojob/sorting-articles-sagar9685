426d990599622741da29cbcf32677966cbe22cd9
sagar9685 <160707983+sagar9685@users.noreply.github.com>
2025-05-03 13:31:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 //your JS code here. If required.
 const bands = [
-      'The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil',
-      'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State',
-      'We Came as Romans', 'Counterparts', 'Oh, Sleeper',
-      'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'
-    ];
+  "The Plot in You",
+  "The Devil Wears Prada",
+  "Pierce the Veil",
+  "Norma Jean",
+  "The Bled",
+  "Say Anything",
+  "The Midway State",
+  "We Came as Romans",
+  "Counterparts",
+  "Oh, Sleeper",
+  "A Skylit Drive",
+  "Anywhere But Here",
+  "An Old Dog",
+];
 function stripArticles(bandName) {
-      return bandName.replace(/^(a |an |the )/i, '').trim();
-    }
+  return bandName.replace(/^(a |an |the )/i, "").trim();
+}
 
 const sortedBands = bands.sort((a, b) => {
-      const nameA = stripArticles(a).toLowerCase();
-      const nameB = stripArticles(b).toLowerCase();
-      return nameA.localeCompare(nameB);
-    });
+  const nameA = stripArticles(a).toLowerCase();
+  const nameB = stripArticles(b).toLowerCase();
+  return nameA.localeCompare(nameB);
+});
 
-
-const bandList = document.getElementById('band');
-    bandList.innerHTML = sortedBands.map(band => `<li>${band}</li>`).join('');
-  
+const bandList = document.getElementById("band");
+bandList.innerHTML = sortedBands
+  .map((band) => `<li>${band}</li><br></center>`)
+  .join("");
